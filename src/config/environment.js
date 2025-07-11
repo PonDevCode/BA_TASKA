afcf9fb89e4bdf0a66e601c9ed41b8a221402788
PonDevCode <pondevcode@gmail.com>
2025-07-11 14:46:57 +0700

    fix deploy Production

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const env = {
     BUILD_MODE: process.env.BUILD_MODE,
--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -1,4 +1,4 @@
-import { env } from "./environment";
+import { env } from "./environment.js";
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
 let taskaDatabaseInstance = null

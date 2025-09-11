24b8e740fcea036d2edfcbf7c2c2877f62701d13
PonDevCode <pondevcode@gmail.com>
2025-09-11 14:51:21 +0700

    update

--- src/services/userSevice.js
+++ src/services/userSevice.js
@@ -166,8 +166,6 @@ const update = async (id, data, userAvatarFile) => {
             // trường hợp upload file lên cloud storage , cụ thể là cloudinary
 
             const uploadResult = await CloudinaryProvider.streamUpload(userAvatarFile.buffer, 'users')
-
-
             // lưu url vào dtb
             updatedUser = await userModel.update(existUser._id, {
                 avatar: uploadResult.secure_url
@@ -183,34 +181,11 @@ const update = async (id, data, userAvatarFile) => {
     } catch (error) { throw error }
 }
 
-// const getDetail = async (id) => {
-//     try {
-//         const board = await boardModel.getDetail(id)
-//         if (!board) {
-//             throw new ApiError(StatusCodes.NOT_FOUND, 'Board Not Found')
-//         }
-//         // cooking data
-//         // b1 clone lại Board
-//         const cloneBoard = cloneDeep(board)
-//         // b2 đẩy card về đúng column của nó 
-//         cloneBoard.columns.forEach(column => {
-//             // equals hàm của mongodb hỗ trợ
-//             column.cards = cloneBoard.cards.filter(card => card.columnId.equals(column._id))
 
-//             // toString hàm của js
-//             // tìm ra cái card có cái columnsId === column._id => gán nó vào mãng card mới của columns
-//             // column.cards = cloneBoard.cards.filter(card => card.columnId?.toString() === column._id?.toString())
-//         })
-//         // b3 xóa Collection cards ko để nó song song với columns
-//         delete cloneBoard.cards
-//         return cloneBoard
-//     } catch (error) { throw error }
-// }
 export const userService = {
     createSeviceUser,
     verifyAccount,
     login,
     refreshToken,
     update
-    // getDetail
 }
